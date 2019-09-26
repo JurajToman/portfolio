a885626efeffa80ab589fd9671010ff5992e6666
Juraj Toman <45455822+JurajToman@users.noreply.github.com>
2019-09-26 22:35:41 +0200

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
+
+// BEGIN ENQUEUE PARENT ACTION
+// AUTO GENERATED - Do not modify or remove comment markers above or below:
+
+if ( !function_exists( 'ms_theme_editor_parent_css' ) ):
+    function ms_theme_editor_parent_css() {
+        wp_enqueue_style( 'chld_thm_cfg_parent', trailingslashit( get_template_directory_uri() ) . 'style.css', array( 'esotera-themefonts' ) );
+    }
+endif;
+add_action( 'wp_enqueue_scripts', 'ms_theme_editor_parent_css', 10 );
+
+// END ENQUEUE PARENT ACTION
